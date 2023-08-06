075fd6566f892019260fe63e3ad70001d05f9bbd
Wilson Dev <wilson.hch@hynkor.com>
2023-08-05 23:46:23 -0500

    Resolviendo las relaciones uno a uno

--- db/models/index.js
+++ db/models/index.js
@@ -5,7 +5,9 @@ function setupModels(sequelize) {
   User.init(UserSchema, User.config(sequelize))
   Customer.init(CustomerSchema, Customer.config(sequelize))
 
+  User.associate(sequelize.models)
   Customer.associate(sequelize.models)
+
 }
 
 module.exports = setupModels
--- db/models/user.model.js
+++ db/models/user.model.js
@@ -33,8 +33,10 @@ const UserSchema = {
 }
 
 class User extends Model{
-  static associate(){
-//
+  static associate(models){
+this.hasOne(models.Customer,{
+  as:'customer',foreignKey:'userId'
+})
   }
   static config(sequelize){
     return {
